acb7fa9cee39150a6371bb494415960495c0bc15
b01027566 <b01027566@M-MXB105C37>
2026-09-22 11:58:18 +0100

    Updated program code with if-else statements.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,10 +1,48 @@
-﻿Console.WriteLine("Please type in the first number followed by the Enter key");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+﻿using System.Xml.Serialization;
 
-Console.WriteLine("Please type in the second number followed by the Enter key");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+CalculatorApp();
+void CalculatorApp()
+{
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
+    int choice = 0;
 
-int result = firstNumber +  secondNumber;
+    Console.WriteLine("Please type in the first number followed by the Enter key");
+     firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("The addition of both numbers is " + result);
-Console.ReadKey();
+    Console.WriteLine("Please type in the second number followed by the Enter key");
+     secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Choose an option from the following list.");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3 - Divide");
+    Console.WriteLine("4 - Multiply");
+
+    choice = Convert.ToInt32(Console.ReadLine());
+    if (choice == 1)
+    {
+        result = firstNumber + secondNumber;
+        Console.WriteLine($"Adding {firstNumber} and {secondNumber} equals {result}");
+    }
+    else if (choice == 2)
+    {
+        result = firstNumber - secondNumber;
+        Console.WriteLine($"Subtratcing {firstNumber} from {secondNumber} equals {result}");
+    }
+    else if (choice == 3)
+    {
+        result = firstNumber / secondNumber;
+        Console.WriteLine($"Dividing {firstNumber} by {secondNumber} equals {result}");
+    }
+    else if (choice == 4)
+    {
+        result = firstNumber * secondNumber;
+        Console.WriteLine($"Multiplying {firstNumber} and {secondNumber} equals {result}");
+    }else
+    {
+        Console.WriteLine("You did not select a valid number between 1-4");
+    }
+        Console.ReadKey();
+}
