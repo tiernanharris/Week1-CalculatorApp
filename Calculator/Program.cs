42196e953a2a84c708eb434ca6609c73973dfe3c
b01027566 <b01027566@M-MXB105C37>
2026-09-22 11:32:16 +0100

    Implemented program code.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please type in the first number followed by the Enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please type in the second number followed by the Enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber +  secondNumber;
+
+Console.WriteLine("The addition of both numbers is " + result);
+Console.ReadKey();
